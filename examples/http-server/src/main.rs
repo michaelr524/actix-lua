f69d88f66a910257ae94eeef350121ed84f4942e
Michael Ravits <michael@xlucidity.com>
2018-08-31 02:27:05 +0300

    pass request to lua, format as string, print to console and return in response

--- examples/http-server/src/main.rs
+++ examples/http-server/src/main.rs
@@ -3,25 +3,52 @@ extern crate actix_lua;
 extern crate actix_web;
 extern crate env_logger;
 extern crate futures;
+extern crate bytes;
 
 use actix::prelude::*;
 use actix_lua::{LuaActor, LuaActorBuilder, LuaMessage};
 use actix_web::{
-    http, middleware, server, App, AsyncResponder, FutureResponse, HttpResponse, Path, State,
+    middleware, server, App, AsyncResponder, FutureResponse, HttpResponse, HttpRequest, HttpMessage
 };
+use std::collections::HashMap;
 use futures::Future;
+use bytes::Bytes;
 
 struct AppState {
     lua: Addr<LuaActor>,
 }
 
-fn index((name, state): (Path<String>, State<AppState>)) -> FutureResponse<HttpResponse> {
-    state
-        .lua
-        .send(LuaMessage::from(name.into_inner()))
+fn build_message(req: &HttpRequest<AppState>, body: &str) -> LuaMessage {
+    let mut headers = HashMap::new();
+
+    for (key, value) in req.headers().iter() {
+        let val_str = value.to_str().expect("header can't be converted to a string");
+        let val_msg = LuaMessage::from(val_str);
+        headers.insert(key.as_str().to_string(), val_msg);
+    }
+
+    let mut t = HashMap::new();
+    t.insert("path".to_string(), LuaMessage::from(req.path()));
+    t.insert("method".to_string(), LuaMessage::from(req.method().to_string()));
+    t.insert("version".to_string(), LuaMessage::from(format!("{:?}", req.version())));
+    t.insert("query_string".to_string(), LuaMessage::from(req.query_string()));
+    t.insert("headers".to_string(), LuaMessage::from(headers));
+    t.insert("body".to_string(), LuaMessage::from(body));
+
+    LuaMessage::from(t)
+}
+
+fn index(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
+    req.body()
         .from_err()
+        .and_then(move |body: Bytes| {  // <- complete body
+            let state = req.state();
+            let message = build_message(&req, std::str::from_utf8(&body).expect("couldn't read body as UTF8 string"));
+
+            state.lua.send(message).from_err()
+        })
         .and_then(|res| match res {
-            LuaMessage::String(s) => Ok(HttpResponse::Ok().json(s)),
+            LuaMessage::String(s) => Ok(HttpResponse::Ok().body(s)),
 
             // ignore everything else
             _ => unimplemented!(),
@@ -38,7 +65,22 @@ fn main() {
         LuaActorBuilder::new()
             .on_handle_with_lua(
                 r#"
-                    return "hi! " .. ctx.msg
+                    local headers = ""
+                    for key,value in pairs(ctx.msg["headers"]) do
+                        headers = headers .. key .. ": " .. value .. "\n"
+                    end
+
+                    local result = ctx.msg["method"] .. " " .. ctx.msg["path"] .. " " .. ctx.msg["version"] .. "\n"
+                            .. "\n"
+                            .. "HTTP headers:\n"
+                            .. headers
+                            .. "\n"
+                            .. "Request body:\n"
+                            .. ctx.msg["body"]
+
+                    print(result)
+
+                    return result
                 "#,
             )
             .build()
@@ -50,7 +92,7 @@ fn main() {
         App::with_state(AppState{lua: addr.clone()})
             // enable logger
             .middleware(middleware::Logger::default())
-            .resource("/{name}", |r| r.method(http::Method::GET).with(index))
+            .default_resource(|r| r.with_async(index))
     }).bind("127.0.0.1:8080")
         .unwrap()
         .start();
